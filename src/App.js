ebabfb6b7c430025cf2a44bbb6c94b9e4154d31e
Henley <henleybailey@googlemail.com>
2020-11-04 18:19:15 +0000

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import BTCList from './components/BTCList';
 import './App.css';
 
 function App() {
+  const [coinData, setCoinData] = useState({});
+  const url = 'https://api.coinranking.com/v1/public/coins?base=GBP&amp;timePeriod=7d';
+
+  // Grab the coin data from CoinRanking.com
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        if (res.ok) {
+          console.log('Success');
+        } else {
+          console.log('Unsuccessful');
+        }
+        return res.json();
+      })
+      .then((json) => {
+        console.log('JSON!', json);
+        setCoinData(json.data);
+      })
+      .catch((error) => console.log('Error:', error));
+    return () => {};
+  }, []);
+
+  console.log(coinData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>AJ Bell - BTC API</h1>
       </header>
+      <section>
+        <h2>BTC List</h2>
+        <BTCList coinData={coinData} />
+      </section>
     </div>
   );
 }
