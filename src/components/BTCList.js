95c0983e53790d52571bc251f18103822a63c949
Henley <henleybailey@googlemail.com>
2020-11-04 22:18:35 +0000

    Removed as refactored

--- src/components/BTCList.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export default function BTCList({ coinData }) {
-  console.log('coin data!', coinData);
-  return <p>{coinData.coins.map((coin) => `<p>${coin.name}</p>`)}</p>;
-}
