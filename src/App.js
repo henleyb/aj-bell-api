10561151787313cf89a30994120f2a7151688168
Henley <henleybailey@googlemail.com>
2020-11-04 22:16:52 +0000

    Added handler

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import BTCList from './components/BTCList';
+import CoinList from './components/CoinList';
+import CoinDetails from './components/CoinDetails';
 import './App.css';
 
 function App() {
-  const [coinData, setCoinData] = useState({});
+  const [coinData, setCoinData] = useState(null);
+  const [selectedCoinID, setSelectedCoinID] = useState(null);
+
   const url = 'https://api.coinranking.com/v1/public/coins?base=GBP&amp;timePeriod=7d';
 
   // Grab the coin data from CoinRanking.com
@@ -25,16 +28,23 @@ function App() {
     return () => {};
   }, []);
 
-  console.log(coinData);
+  console.log('APP!', coinData);
+
+  function handleCoinSelect(id) {
+    console.log('coin clicked!', id);
+    setSelectedCoinID(id);
+  }
 
   return (
     <div className='App'>
       <header className='App-header'>
         <h1>AJ Bell - BTC API</h1>
       </header>
-      <section>
-        <h2>BTC List</h2>
-        <BTCList coinData={coinData} />
+      <h2>Bitcoin and Altcoin List</h2>
+
+      <section className="coinResults">
+        {coinData ? <CoinList coinData={coinData} handleCoinSelect={handleCoinSelect} /> : <p>"No Data"</p>}
+        {coinData ? <CoinDetails coinData={coinData} selectedCoinID={selectedCoinID} /> : <p>"No Data"</p>}
       </section>
     </div>
   );
